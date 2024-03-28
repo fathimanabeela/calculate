e5d165ae0c3c5d6c86791011e3ec8e81e7fe3668
CodeWithNab <codewithnab@gmail.com>
2024-03-28 12:45:26 +0530

    first commit

---
+++ calculate.java
@@ -0,0 +1,12 @@
+public class calculate {
+	public static void main(String[] args) {
+		int num1=3;
+		int num2=5;
+		System.out.println("sum of "+num1+" + "+num2+" is "+ sumOfNumbers(num1,num2));
+	}
+
+	static public int sumOfNumbers(int n1,int n2){
+		int sum=n1+n2;
+		return sum;
+	}
+}
